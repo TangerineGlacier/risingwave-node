1479979bb57e6204325f83b39e1e048a105a0d7b
Sreevatsan <123719399+TangerineGlacier@users.noreply.github.com>
2024-06-19 17:49:12 +0530

    env change

--- index.mjs
+++ index.mjs
@@ -1,16 +1,20 @@
 import express from 'express';
 import pkg from 'pg';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
 
 // Destructure Pool from the default import
 const { Pool } = pkg;
 
-// Database credentials
+// Database credentials from environment variables
 const credentials = {
-  user: 'root',
-  host: 'risingwave.nanoheal.work',
-  database: 'dev',
-  password: '',
-  port: 4567,
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
 };
 
 // Creating a new Express application
@@ -21,7 +25,8 @@ const port = 9000;
 app.get('/test-db', async (req, res) => {
   const pool = new Pool(credentials);
   try {
-    res.json("Connected");
+    const result = await pool.query("select * from source limit 1");
+    res.json(result.rows);
   } catch (error) {
     console.error(error);
     res.status(500).send('Internal Server Error');
